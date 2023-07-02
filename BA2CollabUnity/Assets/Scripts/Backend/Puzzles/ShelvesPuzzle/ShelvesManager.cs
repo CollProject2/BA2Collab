074e806517e9f9f4c1af807131869d7f22bb6c00
Liwady <liwadyv@gmail.com>
2023-07-02 22:48:27 +0200

    shelves manager for the puzzle

--- BA2CollabUnity/Assets/Scripts/Backend/Puzzles/ShelvesPuzzle/ShelvesManager.cs
+++ BA2CollabUnity/Assets/Scripts/Backend/Puzzles/ShelvesPuzzle/ShelvesManager.cs
@@ -4,48 +4,68 @@ using UnityEngine;
 public class ShelvesManager : MonoBehaviour
 {
     public string associatedMemory;
-    public bool isActive;
+    public bool isInteractable;
     public List<Letter> isRight = new();
     public static ShelvesManager instance = null;
     public Letter currentLetter;
+    public List<GameObject> particles;
 
     private void Awake()
     {
         if (instance == null)
             instance = this;
-       // isActive = false;
-
+        isInteractable = false;
     }
+
     private void Update()
     {
-        Player.instance.SetCanMove(false);
-        if (currentLetter != null && isActive) { CheckInput(); }
+        if (currentLetter != null && isInteractable)
+            CheckInput();
+    }
+
+    //call this from the projector 
+    public void InitShelvesAfterProjector()
+    {
+        isInteractable = true;
+        SetParticleSystem(isInteractable);
     }
 
     //set this as the current block
     public void SetCurrentLetter(Letter letter)
     {
+        if(currentLetter == null)
+            Player.instance.SetCanMove(false); 
+        //do ui stuff
+        
         currentLetter = letter;
     }
+
     public void CallCheck()
     {
         if (ShelvesAreSolved())
         {
-            isActive = false;
+            isInteractable = false;
+            SetParticleSystem(isInteractable);
+            Player.instance.SetCanMove(true);
             //recall memory
             Player.instance.RecallMemory(associatedMemory);
+            //activate 3d cube in garden
             Destroy(this);
         }
 
     }
-   
+
+    public void SetParticleSystem(bool enabled)
+    {
+        foreach (var particle in particles) { particle.SetActive(enabled); }
+    }
+
     private void CheckInput()
     {
         if (Input.GetKeyDown(KeyCode.A))
             RotateLetterAt(currentLetter.id, RotationLR.Left);
         else if (Input.GetKeyDown(KeyCode.D))
             RotateLetterAt(currentLetter.id, RotationLR.Right);
-
     }
 
     //rotate the block with the given index in the list
