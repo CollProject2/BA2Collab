0a6441641dd668e29feb2e400bfae3df27660009
yamanefeugurlu <yamanefeugurlu@gmail.com>
2023-07-09 04:18:49 +0200

    added some voice lines to test and startScreen Materials

--- BA2CollabUnity/Assets/Scripts/Frontend/FungusExtension/FmodFungus/FmodFungusTextSound.cs
+++ BA2CollabUnity/Assets/Scripts/Frontend/FungusExtension/FmodFungus/FmodFungusTextSound.cs
@@ -9,58 +9,58 @@ using STOP_MODE = FMOD.Studio.STOP_MODE;
 [CommandInfo("Fmod","textSfx","Play text sound effect while writing")]
 public class FmodFungusTextSound : Command
 {
-    [field: Header("TextSound")]
-    [field: SerializeField] public EventReference textSfx { get; private set; }
-
-    private EventInstance textSfxEvent;
-
-    private PLAYBACK_STATE playbackState;
-
-    public Writer writer;
-
-    public void Start()
-    {
-        textSfxEvent = AudioManager.instance.CreateInstance(textSfx);
-        textSfxEvent.getPlaybackState(out playbackState);
-    }
+    // [field: Header("TextSound")]
+    // [field: SerializeField] public EventReference textSfx { get; private set; }
+    //
+    // private EventInstance textSfxEvent;
+    //
+    // private PLAYBACK_STATE playbackState;
+    //
+    // public Writer writer;
+    //
+    // public void Start()
+    // {
+    //     textSfxEvent = AudioManager.instance.CreateInstance(textSfx);
+    //     textSfxEvent.getPlaybackState(out playbackState);
+    // }
 
     public override void OnEnter()
     {
-        if (writer.IsWriting && !writer.IsWaitingForInput)
-        {
-            if (playbackState.Equals(PLAYBACK_STATE.STOPPED))
-            {
-                textSfxEvent.start();
-            }
-        }
+        // if (writer.IsWriting && !writer.IsWaitingForInput)
+        // {
+        //     if (playbackState.Equals(PLAYBACK_STATE.STOPPED))
+        //     {
+        //         textSfxEvent.start();
+        //     }
+        // }
         Continue();
     }
 
-    private void Update()
-    {
-        textSfxEvent.setParameterByName("TextSoundAge", AudioManager.instance.textSoundAge);
-        
-        if (writer.IsWriting)
-        {
-            if (writer.IsWaitingForInput)
-            {
-                textSfxEvent.stop(STOP_MODE.IMMEDIATE);
-                playbackState = PLAYBACK_STATE.STOPPED;
-            }
-            else if (!writer.IsWaitingForInput)
-            {
-                if (playbackState.Equals(PLAYBACK_STATE.STOPPED))
-                {
-                    textSfxEvent.start();
-                    playbackState = PLAYBACK_STATE.PLAYING;
-                }
-            }
-        }
-        else
-        {
-            textSfxEvent.stop(STOP_MODE.IMMEDIATE);
-            playbackState = PLAYBACK_STATE.STOPPED;
-        }
-        
-    }
+    // private void Update()
+    // {
+    //     textSfxEvent.setParameterByName("TextSoundAge", AudioManager.instance.textSoundAge);
+    //     
+    //     if (writer.IsWriting)
+    //     {
+    //         if (writer.IsWaitingForInput)
+    //         {
+    //             textSfxEvent.stop(STOP_MODE.IMMEDIATE);
+    //             playbackState = PLAYBACK_STATE.STOPPED;
+    //         }
+    //         else if (!writer.IsWaitingForInput)
+    //         {
+    //             if (playbackState.Equals(PLAYBACK_STATE.STOPPED))
+    //             {
+    //                 textSfxEvent.start();
+    //                 playbackState = PLAYBACK_STATE.PLAYING;
+    //             }
+    //         }
+    //     }
+    //     else
+    //     {
+    //         textSfxEvent.stop(STOP_MODE.IMMEDIATE);
+    //         playbackState = PLAYBACK_STATE.STOPPED;
+    //     }
+    //     
+    // }
 }
